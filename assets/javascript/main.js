00da37f261a6de34c962282618f35447fc0f2e0c
FrankV <frank@sabertechnology.com>
2019-01-20 15:14:08 -0500

    additional updates

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -24,6 +24,44 @@ var dataRef = firebase.database();
     var destination = "";
     var startTime = "";
     var frequency = 0;
+    var nextTrain = 0;
+    var tMinutesTillTrain = 0;
+
+    
+function nextArrival(firstTime, tFrequency) {
+
+
+  // var tFrequency = 3;
+  
+  // Time is 3:30 AM
+  var firstTime = "03:30";
+  
+  // First Time (pushed back 1 year to make sure it comes before current time)
+  var firstTimeConverted = moment(firstTime, "HH:mm").subtract(1, "years");
+  console.log(firstTimeConverted);
+  
+  // Current Time
+  var currentTime = moment();
+  console.log("CURRENT TIME: " + currentTime.format("hh:mm"));
+  
+  // Difference between the times
+  var diffTime = currentTime.diff(moment(firstTimeConverted), "minutes");
+  console.log("DIFFERENCE IN TIME: " + diffTime);
+  
+  // Time apart (remainder)
+  var tRemainder = diffTime % tFrequency;
+  console.log(tRemainder);
+  
+  // Minute Until Train
+  var tMinutesTillTrain = tFrequency - tRemainder;
+  console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
+  
+  // Next Train
+  var nextTrain = moment().add(tMinutesTillTrain, "minutes");
+  console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
+  
+    }
+  
 
       // YOUR TASK!!!
       // Code in the logic for storing and retrieving the most recent user.
@@ -57,16 +95,17 @@ var dataRef = firebase.database();
       });
     });
 
-      dataRef.ref().orderByChild("dateAdded").limitToLast(1).on("child_added", function(snapshot) {
+      nextArrival(startTime, frequency);
+
+      dataRef.ref().orderByChild("dateAdded").on("child_added", function(snapshot) {
         // Change the HTML to reflect
         $("#trainName-display").append('<p>' + snapshot.val().trainName);
         $("#destination-display").append('<p>' + snapshot.val().destination);
         $("#frequency-display").append('<p>' + snapshot.val().frequency);
-     
+        $("#nextArrival-display").append('<p>' + nextTrain);
+        $("#minutesAway-display").append('<p>' + tMinutesTillTrain);
+  
       });
 
-    });
 
-
-
-
+});
